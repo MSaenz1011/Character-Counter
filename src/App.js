1ca6eb5586c08d9028380bf398aa6ebaf727e047
MSaenz1011 <michaelsaenz96@hotmail.com>
2023-03-02 14:30:15 -0500

    code improved

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
   const [counter, setCounter] = useState(0);
 
   const handleCheck = (e) => {
-    let currentText = e.target.value;
-    let textExtension = currentText.length;
-    setText(currentText);
+    let textExtension = e.target.value.length;
     setCounter(textExtension);
   };
 
   return (
     <div className="container">
-      <textarea rows="3" value={text} onChange={handleCheck}></textarea>
+      <textarea rows="3" onChange={handleCheck}></textarea>
       <div className="counter">{counter}</div>
     </div>
   );
